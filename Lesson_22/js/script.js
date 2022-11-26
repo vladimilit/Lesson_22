7808e0b9632bfbca0db10088cf5c91866baf4838
vladimilit <65126514+vladimilit@users.noreply.github.com>
2022-11-26 19:50:25 +0200

    Lesson_22

---
+++ Lesson_22/js/script.js
@@ -0,0 +1,20 @@
+console.log("Я учу JS");
+
+let userAge;
+
+userAge = 25;
+
+console.log(userAge);
+
+let user;
+let userName;
+userName = "Вася";
+user = userName;
+
+console.log(user);
+
+let userHeight = 145 / 0;
+console.log(userHeight);
+
+let Username;
+console.log(typeof Username);
